a5c7f9a80b16145c9e0b1a1bfd868338f2a3a40a
ANNE MARCHASSON <annemarchasson@yahoo.fr>
2023-04-14 11:15:21 +0200

    add model.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "./pages/Home";
-
+import Model from "./Model";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <Model />
     </div>
   );
 }
---
+++ frontend/src/Model.jsx
@@ -0,0 +1,5 @@
+function Model() {
+  return <body />;
+}
+
+export default Model;
