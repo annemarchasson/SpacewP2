aef966dae23c49fd94e62d4836adbd0bd6c28419
sax59 <waquetv@gmail.com>
2023-04-18 15:01:24 +0200

    Fix merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,13 @@ import BookingForm from "./components/BookingForm";
 
 function App() {
   return (
-
-  <> 
+    <>
       <BookingForm />
 
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-  </>
+    </>
   );
 }
 export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,7 +44,6 @@ function Cards() {
         : ""}
     </>
   );
-
 }
 
 export default Cards;
