61daf1062a45b8be7dd2d0d0d8ab81b4822e4c83
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2023-04-20 15:44:13 +0200

    addBtnPayment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import BookingForm from "@components/BookingForm/BookingForm";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home";
+import Payment from "@pages/PaymentForm/Payment";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Booking-form" element={<BookingForm />} />
+        <Route path="/Payment-form" element={<Payment/>} />
       </Routes>
     </>
   );
