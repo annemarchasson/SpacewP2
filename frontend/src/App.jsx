145a680d3e218e3279b58a1578d2d24853677b0c
Anthony Senechal <anthony.senechal59128@gmail.com>
2023-04-20 14:42:55 +0200

    navbarmaj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
 import BookingForm from "./components/BookingForm";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
-      <BookingForm />
+      <NavBar />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Booking-form" element={<BookingForm />} />
       </Routes>
     </>
   );
